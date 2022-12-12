9ae7b7be36a37f5f10e154d2a548551981dbb648
manot40 <kevinsandiho290100@gmail.com>
2022-12-12 16:45:21 +0700

    fix: cache control

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,5 +8,8 @@ export const handle: Handle = (async ({ event, resolve }) => {
     }
   });
 
+  // Cache Control
+  response.headers.set('cache-control', 'max-age=0, s-maxage=86400, stale-while-revalidate');
+
   return response;
 }) satisfies Handle;
